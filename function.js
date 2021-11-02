50d9249e0f44f9ef586a028240ddd83003a272cf
swatisukla03 <swatisukla16@gmail.com>
2021-11-02 19:46:25 +0530

    modified

---
+++ function.js
@@ -0,0 +1,15 @@
+var x;
+a();
+b();
+x=3;
+console.log(x);
+ 
+function a(){
+    var x=10;
+    console.log(x);
+
+}
+function b(){
+    var x=100;
+    console.log(x);
+}
--- hoisting.js
+++ hoisting.js
@@ -1,1 +1,10 @@
+getname();
+console.log(a);
 console.log("Welcome to my practice session");
+// practicing the concepts of Hoisting Here!!
+var a=6;
+function getname(){
+    console.log("Nmaste Javascript");
+}
+// getname();
+// console.log(a);
